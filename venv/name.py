71bbd0c894f90a1c5bc04879a7a9832e166c0bc2
Danik <mlokodan4@gmail.com>
2022-11-16 21:37:34 +0200

    changes1

---
+++ venv/name.py
@@ -0,0 +1,4 @@
+import datetime
+zminna_date_time=datetime.datetime(2022,11,16,hour=21,minute=35,second=34,microsecond=585)
+print(f'object datetime-{zminna_date_time}')
+print(f'type-{type(zminna_date_time)}')
